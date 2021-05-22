97e0dbd10a9a460dba70b6be97cb03e45547094c
fortuitous-prototype-dfa5 <lindsay.chen@yale.edu>
2021-05-22 10:00:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,15 +2,16 @@ def addition(num1, num2)
   sum = num1.to_i + num2.to_i
   puts sum
 end
-
 addition "2", "3"
 
 def subtraction(num1, num2)
-
+  sub = num1.to_i - num2.to_i
+  puts sub
 end
+subtraction "10", "5"
 
 def division(num1, num2)
-
+  div = num1.to_i 
 end
 
 def multiplication(num1, num2)
